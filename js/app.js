7c172285102cde34ecc90f8ef5135bb820e3c56e
Fsesnick <fernandasesnick@gmail.com>
2021-02-02 10:11:27 +0000

    Changing styles and classes

--- js/app.js
+++ js/app.js
@@ -10,4 +10,5 @@ const $pdfs = $('a[href$=".pdf"]');
 $secureLinks.attr('target', '_blank');
 $pdfs.attr('download', true);
 
-$odd.css('backgroundColor', 'lightgrey');
+$secureLinks.addClass('secure');
+$pdfs.addClass('pdf');
