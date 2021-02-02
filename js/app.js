a80a4ee5da1f1849950c4006ba574d4b2266bfcb
Fsesnick <fernandasesnick@gmail.com>
2021-02-02 10:53:06 +0000

    unobtrusive

--- js/app.js
+++ js/app.js
@@ -1,11 +1,7 @@
 const $odd = $('a:odd');
-//odd.hide();
-
 const $secureLinks = $('a[href^="https://"]');
-//secureLinks.hide();
-
 const $pdfs = $('a[href$=".pdf"]');
-//$pdfs.hide();
+const $pdfCheckbox = $('<label><input type="checkbox"> Allow PDF downloads</label>');
 
 $secureLinks.attr('target', '_blank');
 $pdfs.attr('download', true);
@@ -14,17 +10,10 @@ $secureLinks.addClass('secure');
 $pdfs.addClass('pdf');
 
 $pdfs.on('click', function(event){
- // event.preventDefault();
-  
-  // verificar se a caixa de seleção está marcada  
-  //0 checkboxes marcados, Se não estiver marcada
   if ($(':checked').length === 0){
-    
-    //  evitaro download do documento
-     event.preventDefault();
-     //  alertar o usuário
-  alert('Marque a caixa para permitir downloads de PDF.');
-  }     
-     
-  // else acionar o download
+    event.preventDefault();
+    alert('Please check the box to allow PDF downloads.');
+  }
 });
+
+$('#links').append($pdfCheckbox);
